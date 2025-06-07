bfd14f5a8c777daab4be9457d3b7986e3155b4b7
Kepler183f <rajdeepvilluri@gmail.com>
2025-06-07 09:58:26 -0400

    added JPA integration and replaced in-memory database with AccountRepository

--- src/main/java/com/rajdeep/accounts_service/AccountService.java
+++ src/main/java/com/rajdeep/accounts_service/AccountService.java
@@ -1,19 +1,35 @@
 package com.rajdeep.accounts_service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.Map;
 import java.util.Optional;
-
+import java.util.UUID;
 @Service
 public class AccountService {
 
-    // Our temporary, in-memory database. A Map of AccountID -> Account Object.
-    private static final Map<String, Account> FAKE_DATABASE = Map.of(
-            "123", new Account("123", "CUST-456", "CHECKING", 5450.75),
-            "456", new Account("456", "CUST-789", "SAVINGS", 12345.00)
-    );
+
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository)
+    {
+        this.accountRepository = accountRepository;
+
+    }
+
+
 
     // Business logic to find an account by its ID
     public Optional<Account> findAccountById(String accountId) {
-        return Optional.ofNullable(FAKE_DATABASE.get(accountId));
+        return accountRepository.findById(accountId);
     }
+
+    public Account createAccount(Account newAccount)
+    {
+        String newAccountId = UUID.randomUUID().toString();
+        newAccount.setAccountID(newAccountId);
+
+        // The save method handles creating the new record in the database
+        return accountRepository.save(newAccount);
+    }
+
 }
