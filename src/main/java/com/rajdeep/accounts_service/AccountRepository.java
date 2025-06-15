21ed1c3a376002616ef29244ea35d9e78131b139
Kepler183f <rajdeepvilluri@gmail.com>
2025-06-14 23:05:23 -0400

    added a method to find accounts by owner ID

--- src/main/java/com/rajdeep/accounts_service/AccountRepository.java
+++ src/main/java/com/rajdeep/accounts_service/AccountRepository.java
@@ -1,12 +1,12 @@
 package com.rajdeep.accounts_service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
 
+import java.util.List; // Add this import
+
 @Repository
-
-public interface AccountRepository extends JpaRepository<Account, String>
-{
-
+public interface AccountRepository extends JpaRepository<Account, String> {
+    // Add this method to find accounts by ownerID
+    List<Account> findByOwnerID(String ownerID);
 }
